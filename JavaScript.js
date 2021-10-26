bba14ae4acc9f548d10ba516594181062eb1994f
RickyOliveira <contactricky@zoho.com>
2021-10-26 17:37:26 -0300

    rotnav

--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,6 @@
-const panels = document.querySelectorAll(".panel") /*  creating an array with all the classes that contain "panel"  */
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+const container = document.querySelector(".container")
 
-panels.forEach ( (panel) => {
-    panel.addEventListener ("click", () => {
-        removeActiveClasses ()
-        panel.classList.add ("active")
-    })
-})
-
-function removeActiveClasses () {
-    panels.forEach ((panel) =>{
-        panel.classList.remove ("active")
-    })}
+open.addEventListener ("click" , () => container.classList.add("show-nav"))
+close.addEventListener ("click" , () => container.classList.remove("show-nav"))
